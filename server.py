9669fa16ae80177ae31084f26261ea3d63406fe3
Nathan Stehr <nstehr@gmail.com>
2013-04-13 11:44:08 -0400

    server changes to server current content

--- server.py
+++ server.py
@@ -1,8 +1,21 @@
 import os
+import flask
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return flask.send_from_directory('MovieFinder/WebContent/','index.html')
+
+@app.route('/WEB-INF/lib/<path:filename>')
+def libs(filename):
+    return flask.send_from_directory('MovieFinder/WebContent/WEB-INF/lib',filename)
+
+
+@app.route('/styles/<path:filename>')
+def styles(filename):
+    return flask.send_from_directory('MovieFinder/WebContent/styles',filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
